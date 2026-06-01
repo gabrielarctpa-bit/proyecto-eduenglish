ae0e8d29112b3030da09f566deb35673a7ca6022
gabrielarctpa-bit <gabrielar.ctpa@gmail.com>
2026-06-01 10:06:11 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def inicio():
-    return send_from_directory('.', '/html/index.html')
+    return send_from_directory('.', 'index.html')
 
 @app.route('/css/<path:archivo>')
 def css(archivo):
